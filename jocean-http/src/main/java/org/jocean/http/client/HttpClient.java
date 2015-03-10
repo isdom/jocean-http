90c717d987f9314eef4e3a85334754b2c0d2f029
ming.ma <isdom.maming@gmail.com>
2015-03-10 13:38:46 +0800

    change appendContent's return type, from void ==> HttpClient

--- jocean-http/src/main/java/org/jocean/http/client/HttpClient.java
+++ jocean-http/src/main/java/org/jocean/http/client/HttpClient.java
@@ -18,8 +18,21 @@ import rx.Observable;
  */
 public interface HttpClient extends Closeable {
 	
-	public void appendContent(final HttpContent content);
+	/**
+	 * 添加可发送的HttpContent作为HttpRequest的补充,
+	 * 当sendRequest未被调用前content会被缓存,而sendRequest如已经被调用
+	 * 则content会立即通过HttpClient实例被发送
+	 * @param content 要发送的HttpContent实例
+	 * @return HttpClient实例,可用于链式表达式
+	 */
+	public HttpClient appendContent(final HttpContent content);
 	
+	/**
+	 * 通过Httpclient实例发送Http请求
+	 * @param request 要发送的HttpRequest
+	 * @return Observable<HttpObject> Observable of HttpObject, 
+	 * 推送内容为 HttpResponse + 0~N (HttpContent)
+	 */
 	public Observable<HttpObject> sendRequest(final HttpRequest request);
 	
 	public interface Builder {
