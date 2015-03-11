716dee0186d04aa46a80afc9f5764305c998f4b6
ming.ma <isdom.maming@gmail.com>
2015-03-11 18:33:50 +0800

    implement HttpClientBuilder as DefaultClientBuilder, and create client's Testcase with HttpTestServer

---
+++ jocean-http/src/test/java/org/jocean/http/server/HttpTestServer.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2012 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package org.jocean.http.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+/**
+ * An HTTP server that sends back the content of the received HTTP request
+ * in a pretty plaintext form.
+ */
+public final class HttpTestServer {
+
+    public HttpTestServer(final boolean enableSSL, final int port) throws Exception {
+        // Configure SSL.
+        final SslContext sslCtx;
+        if (enableSSL) {
+            SelfSignedCertificate ssc = new SelfSignedCertificate();
+            sslCtx = SslContext.newServerContext(ssc.certificate(), ssc.privateKey());
+        } else {
+            sslCtx = null;
+        }
+
+        // Configure the server.
+        _bossGroup = new NioEventLoopGroup(1);
+        _workerGroup = new NioEventLoopGroup();
+        
+        ServerBootstrap b = new ServerBootstrap();
+        b.option(ChannelOption.SO_BACKLOG, 1024);
+        b.group(_bossGroup, _workerGroup)
+         .channel(NioServerSocketChannel.class)
+         .handler(new LoggingHandler(LogLevel.INFO))
+         .childHandler(new HttpTestServerInitializer(sslCtx));
+
+        Channel ch = b.bind(port).sync().channel();
+
+        System.err.println("Open your web browser and navigate to " +
+                (enableSSL? "https" : "http") + "://127.0.0.1:" + port + '/');
+    }
+    
+    public void stop() {
+        _bossGroup.shutdownGracefully();
+        _workerGroup.shutdownGracefully();
+    }
+
+    private final EventLoopGroup _bossGroup;
+    private final EventLoopGroup _workerGroup;
+    
+    public static void main(String[] args) throws Exception {
+    	@SuppressWarnings("unused")
+		final HttpTestServer server = new HttpTestServer(false, 8080);
+    }
+}
