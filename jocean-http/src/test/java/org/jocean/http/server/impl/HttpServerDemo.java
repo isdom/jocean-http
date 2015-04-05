d944437bc3496338411f447da28108573b2922a0
ming.ma <isdom.maming@gmail.com>
2015-04-06 00:03:18 +0800

    改进HttpServerDemo例子，在额外线程中阻塞读取HttpServer接收到的Http请求内容

--- jocean-http/src/test/java/org/jocean/http/server/impl/HttpServerDemo.java
+++ jocean-http/src/test/java/org/jocean/http/server/impl/HttpServerDemo.java
@@ -1,7 +1,5 @@
 package org.jocean.http.server.impl;
 
-import java.util.Iterator;
-
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -11,25 +9,39 @@ import io.netty.channel.local.LocalAddress;
 import io.netty.channel.local.LocalEventLoopGroup;
 import io.netty.channel.local.LocalServerChannel;
 import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaders;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+import java.io.IOException;
+import java.util.Iterator;
 
 import org.jocean.event.api.EventEngine;
 import org.jocean.event.extend.Runners;
 import org.jocean.event.extend.Services;
+import org.jocean.http.HttpFeature;
 import org.jocean.http.client.impl.DefaultHttpClient;
 import org.jocean.http.client.impl.TestChannelCreator;
 import org.jocean.http.server.HttpServer;
 import org.jocean.http.util.RxNettys;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import rx.Observable;
 import rx.functions.Action1;
-import rx.functions.Func1;
 
 public class HttpServerDemo {
+    
+    private static final Logger LOG =
+            LoggerFactory.getLogger(HttpServerDemo.class);
 
     public static void main(final String[] args) throws Exception {
+        SelfSignedCertificate ssc = new SelfSignedCertificate();
+        final SslContext sslCtx = SslContext.newServerContext(ssc.certificate(), ssc.privateKey());
+
         final EventEngine engine = 
                 Runners.build(new Runners.Config()
                     .objectNamePrefix("demo:type=test")
@@ -48,37 +60,51 @@ public class HttpServerDemo {
                 bootstrap.channel(LocalServerChannel.class);
             }});
         
-        final Iterator<HttpObject> itr = 
         server.create(new LocalAddress("test"))
             .doOnNext(new Action1<Channel>() {
                 @Override
                 public void call(final Channel channel) {
                     InboundFeature.CONTENT_COMPRESSOR.applyTo(channel);
-                    InboundFeature.CLOSE_ON_IDLE.applyTo(channel, 180);
+                    InboundFeature.ENABLE_SSL.applyTo(channel, sslCtx);
+                    InboundFeature.CLOSE_ON_IDLE.applyTo(channel, 1);
                     InboundFeature.LOGGING.applyTo(channel);
                 }})
-            .flatMap(new Func1<Channel, Observable<HttpObject>>() {
+            .subscribe(new Action1<Channel>() {
                 @Override
-                public Observable<HttpObject> call(final Channel channel) {
-                    return new DefaultHttpInbound(channel, engine).request();
-                }})
-            .map(RxNettys.<HttpObject>retainMap())
-            .toBlocking().toIterable().iterator();
+                public void call(final Channel channel) {
+                  final Iterator<HttpObject> itr = 
+                    new DefaultHttpInbound(channel, engine).request()
+                    .map(RxNettys.<HttpObject>retainMap())
+                    .toBlocking().toIterable().iterator();
+                  
+                  new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            final byte[] bytes = RxNettys.httpObjectsAsBytes(itr);
+                            LOG.info("recv {}", new String(bytes, "UTF-8"));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }}).start();
+                }});
         
-        {
+        while (true) {
             final ByteBuf content = Unpooled.buffer(0);
             content.writeBytes("test content".getBytes("UTF-8"));
             final DefaultFullHttpRequest request = 
                     new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, "/", content);
+            HttpHeaders.setContentLength(request, content.readableBytes());
             
+            @SuppressWarnings("resource")
             final DefaultHttpClient client = new DefaultHttpClient(new TestChannelCreator());
             client.sendRequest(
                 new LocalAddress("test"), 
-                Observable.just(request))
+                Observable.just(request),
+                HttpFeature.EnableLOG,
+                HttpFeature.EnableSSL)
                 .subscribe();
+            Thread.sleep(1000);
         }
-                
-        final byte[] bytes = RxNettys.httpObjectsAsBytes(itr);
-        System.out.println(new String(bytes, "UTF-8"));
     }
 }
