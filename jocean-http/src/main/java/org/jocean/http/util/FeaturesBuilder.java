c02eb71a6dc655f8c0666dc5f4804e88b1bacc59
ming.ma <isdom.maming@gmail.com>
2015-08-16 14:19:20 +0800

    添加 FeaturesBuilder 工具类, 用于产生 Feature[]

---
+++ jocean-http/src/main/java/org/jocean/http/util/FeaturesBuilder.java
@@ -0,0 +1,21 @@
+package org.jocean.http.util;
+
+import java.util.List;
+
+import org.jocean.http.Feature;
+
+import rx.functions.Func0;
+
+public class FeaturesBuilder implements Func0<Feature[]> {
+    
+    public FeaturesBuilder(final List<Feature> features) {
+        this._features = features;
+    }
+
+    @Override
+    public Feature[] call() {
+        return this._features.toArray(Feature.EMPTY_FEATURES);
+    }
+
+    private final List<Feature> _features;
+}
