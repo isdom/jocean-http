cc6d00c0dad5dbc8915de587f1f91e26c60b67b2
ming.ma <isdom.maming@gmail.com>
2016-02-23 14:10:01 +0800

    clear code

--- jocean-http/src/main/java/org/jocean/http/client/Outbound.java
+++ jocean-http/src/main/java/org/jocean/http/client/Outbound.java
@@ -13,48 +13,12 @@ public class Outbound {
     public interface ApplyToRequest extends Action1<HttpRequest> {
     }
     
-//    public interface ResponseSubscriberAware {
-//        public void setResponseSubscriber(final Subscriber<Object> subscriber);
-//    }
-    
     public static final Feature ENABLE_MULTIPART = new Feature.AbstractFeature0() {
         @Override
         public String toString() {
             return "ENABLE_MULTIPART";
         }
     };
-    
-    /*
-    public static final class ENABLE_PROGRESSIVE implements 
-        Feature, ResponseSubscriberAware, Cloneable {
-        public ENABLE_PROGRESSIVE(final long minIntervalInMs) {
-            this._minIntervalInMs = minIntervalInMs;
-        }
-        
-        @Override
-        public void setResponseSubscriber(Subscriber<Object> subscriber) {
-            this._responseSubscriber = subscriber;
-        }
-        
-        @Override
-        public ChannelHandler call(final HandlerBuilder builder, final ChannelPipeline pipeline) {
-            return builder.build(this, pipeline, this._responseSubscriber, this._minIntervalInMs);
-        }
-        
-        @Override
-        public ENABLE_PROGRESSIVE clone() throws CloneNotSupportedException {
-            return (ENABLE_PROGRESSIVE)super.clone();
-        }
-
-        @Override
-        public String toString() {
-            return "ENABLE_PROGRESSIVE";
-        }
-        
-        private final long _minIntervalInMs;
-        private Subscriber<Object> _responseSubscriber = null;
-    }
-    */
     
     public interface InteractionMeterFeature extends Feature, InteractionMeter {
     }
