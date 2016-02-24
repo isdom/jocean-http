c20879fde3c196d44f3fda32e9f2b0dccb60be22
ming.ma <isdom.maming@gmail.com>
2016-02-24 18:02:33 +0800

    增加 buildPayloadCounterFeature 方法，产生 PayloadCounterProxy 实例

--- jocean-http/src/main/java/org/jocean/http/client/impl/HttpClientUtil.java
+++ jocean-http/src/main/java/org/jocean/http/client/impl/HttpClientUtil.java
@@ -3,7 +3,12 @@ package org.jocean.http.client.impl;
 import org.jocean.http.client.Outbound;
 
 public class HttpClientUtil {
+    
     public static Outbound.TrafficCounterFeature buildTrafficCounterFeature() {
         return new TrafficCounterProxy();
+    }
+
+    public static Outbound.PayloadCounterFeature buildPayloadCounterFeature() {
+        return new PayloadCounterProxy();
     }
 }
