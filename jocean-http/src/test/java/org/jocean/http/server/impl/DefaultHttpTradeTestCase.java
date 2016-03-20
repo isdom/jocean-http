58887d2cbe8ff19aa3e20bc55ca4eab320114a47
ming.ma <isdom.maming@gmail.com>
2016-03-20 21:06:25 +0800

    添加测试用例: 正常的 HttpRequest & HttpResponse 形成的一次交互

--- jocean-http/src/test/java/org/jocean/http/server/impl/DefaultHttpTradeTestCase.java
+++ jocean-http/src/test/java/org/jocean/http/server/impl/DefaultHttpTradeTestCase.java
@@ -1,5 +1,7 @@
 package org.jocean.http.server.impl;
 
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -17,7 +19,9 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.local.LocalChannel;
 import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpVersion;
@@ -68,7 +72,7 @@ public class DefaultHttpTradeTestCase {
     }
 
     @Test
-    public final void tesTradeForCompleteRound() throws Exception {
+    public final void tesTradeForCompleteRequestAndErrorResponse() throws Exception {
         final ByteBuf content = Unpooled.buffer(0);
         content.writeBytes("test content".getBytes("UTF-8"));
         final DefaultFullHttpRequest request = 
@@ -97,6 +101,48 @@ public class DefaultHttpTradeTestCase {
         assertNotNull(fullrequest);
         
         Observable.<HttpObject>error(new RuntimeException("ResponseError"))
+            .subscribe(trade.responseObserver());
+        
+        assertEquals(2, request.refCnt());
+        
+        fullrequest.release();
+        
+        assertFalse(trade.isActive());
+        assertEquals(1, request.refCnt());
+    }
+
+    @Test
+    public final void tesTradeForCompleteRound() throws Exception {
+        final ByteBuf content = Unpooled.buffer(0);
+        content.writeBytes("test content".getBytes("UTF-8"));
+        final DefaultFullHttpRequest request = 
+                new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.POST, "/", content);
+        
+        assertEquals(1, request.refCnt());
+        
+        final AtomicReference<Subscriber<? super HttpObject>> subscriberRef = new AtomicReference<>();
+        
+        final DefaultHttpTrade trade = new DefaultHttpTrade(new LocalChannel(), 
+                Observable.create(new OnSubscribe<HttpObject>() {
+                    @Override
+                    public void call(final Subscriber<? super HttpObject> t) {
+                        subscriberRef.set(t);
+                    }}));
+        
+        CachedRequest cached = new CachedRequest(trade);
+        
+        subscriberRef.get().onNext(request);
+        subscriberRef.get().onCompleted();
+        
+        assertTrue(trade.isActive());
+        assertEquals(2, request.refCnt());
+        
+        final FullHttpRequest fullrequest = cached.retainFullHttpRequest();
+        assertNotNull(fullrequest);
+        
+        final FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK);
+        
+        Observable.<HttpObject>just(response)
             .subscribe(trade.responseObserver());
         
         assertEquals(2, request.refCnt());
