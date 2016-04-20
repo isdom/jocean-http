98a6cb2ee44ba50c48a46ff615e70bb23cebd6c3
ming.ma <isdom.maming@gmail.com>
2016-04-20 13:51:25 +0800

    去掉废弃接口 HttpObjectObserverAware

--- jocean-http/src/main/java/org/jocean/http/server/impl/HttpObjectObserverAware.java
+++
@@ -1,8 +0,0 @@
-package org.jocean.http.server.impl;
-
-import io.netty.handler.codec.http.HttpObject;
-import rx.Observer;
-
-public interface HttpObjectObserverAware {
-    public void setHttpObjectObserver(final Observer<HttpObject> observer);
-}
