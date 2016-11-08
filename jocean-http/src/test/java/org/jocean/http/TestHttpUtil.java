f8f6db92313565219a4aaad8189b38a5d7e654b4
ming.ma <isdom.maming@gmail.com>
2016-11-08 17:42:31 +0800

    fix compile warn for netty-all for 4.1.x

--- jocean-http/src/test/java/org/jocean/http/TestHttpUtil.java
+++ jocean-http/src/test/java/org/jocean/http/TestHttpUtil.java
@@ -1,6 +1,5 @@
 package org.jocean.http;
 
-import static io.netty.handler.codec.http.HttpHeaders.Names.CONTENT_TYPE;
 import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 
 import org.jocean.http.server.HttpServerBuilder;
@@ -14,13 +13,13 @@ import org.jocean.idiom.rx.RxActions;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelOption;
+import io.netty.channel.DefaultEventLoopGroup;
 import io.netty.channel.local.LocalAddress;
-import io.netty.channel.local.LocalEventLoopGroup;
 import io.netty.channel.local.LocalServerChannel;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
-import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpVersion;
 import rx.Observable;
@@ -32,7 +31,7 @@ import rx.functions.Func0;
 public class TestHttpUtil {
     private final static HttpServerBuilder TEST_SERVER_BUILDER = new DefaultHttpServerBuilder(
             new AbstractBootstrapCreator(
-            new LocalEventLoopGroup(1), new LocalEventLoopGroup()) {
+            new DefaultEventLoopGroup(1), new DefaultEventLoopGroup()) {
         @Override
         protected void initializeBootstrap(final ServerBootstrap bootstrap) {
             bootstrap.option(ChannelOption.SO_BACKLOG, 1024);
@@ -79,8 +78,8 @@ public class TestHttpUtil {
         final FullHttpResponse response = new DefaultFullHttpResponse(
                 HttpVersion.HTTP_1_1, OK, 
                 Unpooled.wrappedBuffer(bodyAsBytes));
-        response.headers().set(CONTENT_TYPE, contentType);
-        response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, response.content().readableBytes());
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE, contentType);
+        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
         return  Observable.<HttpObject>just(response);
     }
 }
