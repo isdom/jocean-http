355e5def703bde451301354e2f2afa239ed80754
ming.ma <isdom.maming@gmail.com>
2016-12-30 14:31:20 +0800

    fix javadoc for HttpClient

--- jocean-http/src/main/java/org/jocean/http/client/HttpClient.java
+++ jocean-http/src/main/java/org/jocean/http/client/HttpClient.java
@@ -21,11 +21,11 @@ public interface HttpClient extends Closeable {
     
     /**
      * 定义一次与http server的交互, 指定了远端地址和可选特性，并通过 requestProvider 创建 request
-     * 当返回的Observable<? extends Object> 实例被subscribe时,才基于上述指定的参数真正发起交互动作
+     * 当返回的Observable<? extends HttpObject> 实例被subscribe时,才基于上述指定的参数真正发起交互动作
      * @param remoteAddress 远端地址
      * @param requestProvider 要发送的HttpRequest (Object)* 的发生器，创建的 request 可使用传入的 DoOnUnsubscribe
      *          实例，控制 request 相关资源的释放，该 DoOnUnsubscribe 实例确保和特定的一次交互生命周期一致
-     * @return Observable<Object> response: Observable of HttpObject, 
+     * @return Observable<HttpObject> response: Observable of HttpObject, 
      * 推送内容为 HttpResponse + 0~N (HttpContent)
      */
     public Observable<? extends HttpObject> defineInteraction(
@@ -36,10 +36,10 @@ public interface HttpClient extends Closeable {
     
     /**
      * 定义一次与http server的交互, 指定了远端地址和要发送的request和可选特性
-     * 当返回的Observable<? extends Object> 实例被subscribe时,才基于上述指定的参数真正发起交互动作
+     * 当返回的Observable<? extends HttpObject> 实例被subscribe时,才基于上述指定的参数真正发起交互动作
      * @param remoteAddress 远端地址
      * @param request 要发送的HttpRequest (Object)*
-     * @return Observable<Object> response: Observable of HttpObject, 
+     * @return Observable<HttpObject> response: Observable of HttpObject, 
      * 推送内容为 HttpResponse + 0~N (HttpContent)
      */
     public Observable<? extends HttpObject> defineInteraction(
