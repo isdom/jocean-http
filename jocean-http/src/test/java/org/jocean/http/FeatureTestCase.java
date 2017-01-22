757d9f970c18134a0d0172f7aa6aa4681250051c
ming.ma <isdom.maming@gmail.com>
2017-01-22 18:48:55 +0800

    新增 测试用例

--- jocean-http/src/test/java/org/jocean/http/FeatureTestCase.java
+++ jocean-http/src/test/java/org/jocean/http/FeatureTestCase.java
@@ -1,7 +1,10 @@
 package org.jocean.http;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertSame;
 
+import org.jocean.idiom.InterfaceUtils;
 import org.junit.Test;
 
 public class FeatureTestCase {
@@ -24,5 +27,25 @@ public class FeatureTestCase {
         final Feature[] unioned3 = Feature.Util.union(
                 new Feature[]{Feature.ENABLE_LOGGING, Feature.ENABLE_COMPRESSOR}, Feature.ENABLE_COMPRESSOR);
         assertEquals(2, unioned3.length);
+    }
+
+    @Test
+    public void testFeaturesClass() {
+        System.out.println(Feature.FLUSH_PER_WRITE.getClass());
+        
+        assertNotEquals(Feature.ENABLE_LOGGING.getClass(), Feature.ENABLE_LOGGING_OVER_SSL.getClass());
+        assertNotEquals(Feature.ENABLE_LOGGING.getClass(), Feature.FLUSH_PER_WRITE.getClass());
+    }
+
+    @Test
+    public void testFeaturesSelectIncludeType() {
+        final Feature[] features = new Feature[]{Feature.ENABLE_LOGGING, Feature.FLUSH_PER_WRITE, Feature.ENABLE_LOGGING_OVER_SSL};
+                
+        final Object[] flushPerWriteFeature = 
+                InterfaceUtils.selectIncludeType(
+                        Feature.FLUSH_PER_WRITE.getClass(), (Object[])features);
+        
+        assertEquals(1, flushPerWriteFeature.length);
+        assertSame(flushPerWriteFeature[0], Feature.FLUSH_PER_WRITE);
     }
 }
