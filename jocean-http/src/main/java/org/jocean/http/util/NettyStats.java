4506c31037cf16f8203981a2ce45181872ab49a9
ming.ma <isdom.maming@gmail.com>
2017-01-23 21:59:15 +0800

    rename NettyMBean --> NettyStats, nettymbean.xml --> nettystats.xml

--- jocean-http/src/main/java/org/jocean/http/util/NettyStats.java
+++ jocean-http/src/main/java/org/jocean/http/util/NettyStats.java
@@ -2,7 +2,7 @@ package org.jocean.http.util;
 
 import io.netty.buffer.PooledByteBufAllocator;
 
-public class NettyMBean {
+public class NettyStats {
     public String getPooledByteBufAllocatorStats() {
         return PooledByteBufAllocator.DEFAULT.dumpStats();
     }
