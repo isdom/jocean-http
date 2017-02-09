fe1a91d5bc272f7b228dec662125e356a02f35d5
ming.ma <isdom.maming@gmail.com>
2017-02-09 10:56:29 +0800

    添加工具类方法 fromByteArray 从 byte[] 产生 Blob 实例

--- jocean-http/src/main/java/org/jocean/netty/BlobRepo.java
+++ jocean-http/src/main/java/org/jocean/netty/BlobRepo.java
@@ -1,5 +1,6 @@
 package org.jocean.netty;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 import io.netty.util.ReferenceCounted;
@@ -25,6 +26,74 @@ public interface BlobRepo {
 
         @Override
         Blob touch(Object hint);
+        
+        public static class Util {
+            public static Blob fromByteArray(
+                    final byte[] content, 
+                    final String contentType,
+                    final String filename,
+                    final String name) {
+                return new Blob() {
+                    @Override
+                    public String toString() {
+                        final StringBuilder builder = new StringBuilder();
+                        builder.append("Blob [name=").append(name())
+                            .append(", filename=").append(filename())
+                            .append(", contentType=").append(contentType())
+                            .append(", content.length=").append(content.length)
+                            .append("]");
+                        return builder.toString();
+                    }
+                    @Override
+                    public String name() {
+                        return name;
+                    }
+                    @Override
+                    public String filename() {
+                        return filename;
+                    }
+                    @Override
+                    public String contentType() {
+                        return contentType;
+                    }
+                    @Override
+                    public int refCnt() {
+                        return 1;
+                    }
+                    @Override
+                    public Blob retain() {
+                        return this;
+                    }
+                    @Override
+                    public Blob retain(int increment) {
+                        return this;
+                    }
+                    @Override
+                    public Blob touch() {
+                        return this;
+                    }
+                    @Override
+                    public Blob touch(Object hint) {
+                        return this;
+                    }
+                    @Override
+                    public boolean release() {
+                        return false;
+                    }
+                    @Override
+                    public boolean release(int decrement) {
+                        return false;
+                    }
+                    @Override
+                    public InputStream inputStream() {
+                        return new ByteArrayInputStream(content);
+                    }
+                    @Override
+                    public int contentLength() {
+                        return content.length;
+                    }};
+            }
+        }
     }
     
     public Observable<String> putBlob(
