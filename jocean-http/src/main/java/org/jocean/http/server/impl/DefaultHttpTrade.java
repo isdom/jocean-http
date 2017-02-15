4eed8292849f60521e47efcf5f17e49b058f4d64
ming.ma <isdom.maming@gmail.com>
2017-02-15 09:56:36 +0800

    DefaultHttpTrade 基于 _id 重载 equals & hashCode

--- jocean-http/src/main/java/org/jocean/http/server/impl/DefaultHttpTrade.java
+++ jocean-http/src/main/java/org/jocean/http/server/impl/DefaultHttpTrade.java
@@ -61,6 +61,34 @@ class DefaultHttpTrade implements HttpTrade,  Comparable<DefaultHttpTrade>  {
         return this._id - o._id;
     }
     
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + _id;
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DefaultHttpTrade other = (DefaultHttpTrade) obj;
+        if (_id != other._id)
+            return false;
+        return true;
+    }
+
     @Override
     public String toString() {
         final StringBuilder builder = new StringBuilder();
