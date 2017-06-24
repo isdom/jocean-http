85d5210160cd3a264c0a0434974ec70d8e4462ec
ming.ma <isdom.maming@gmail.com>
2017-06-24 17:24:55 +0800

    简化 startInteraction 入参

--- jocean-http/src/test/java/org/jocean/http/client/impl/DefaultHttpClientTestCase.java
+++ jocean-http/src/test/java/org/jocean/http/client/impl/DefaultHttpClientTestCase.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
-import java.net.SocketAddress;
 import java.security.cert.CertificateException;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -18,8 +17,8 @@ import javax.net.ssl.SSLException;
 import org.jocean.http.Feature;
 import org.jocean.http.Feature.ENABLE_SSL;
 import org.jocean.http.TestHttpUtil;
-import org.jocean.http.client.HttpClient;
 import org.jocean.http.client.HttpClient.HttpInitiator;
+import org.jocean.http.client.HttpClient.InitiatorBuilder;
 import org.jocean.http.server.HttpServerBuilder.HttpTrade;
 import org.jocean.http.util.HttpMessageHolder;
 import org.jocean.http.util.Nettys;
@@ -151,13 +150,10 @@ public class DefaultHttpClientTestCase {
     }
     
     private static HttpInitiator startInteraction(
-            final HttpClient client,
-            final SocketAddress addr, 
+            final InitiatorBuilder builder,
             final Observable<? extends Object> request, 
             final Interaction interaction ) throws Exception {
-        try ( final HttpInitiator initiator = client.initiator()
-                .remoteAddress(addr)
-                .build()
+        try ( final HttpInitiator initiator = builder.build()
                 .toBlocking()
                 .single()) {
             final HttpMessageHolder holder = new HttpMessageHolder();
@@ -191,8 +187,7 @@ public class DefaultHttpClientTestCase {
                 new DefaultHttpClient(new TestChannelCreator(), 
                 Feature.ENABLE_LOGGING);
         try {
-            startInteraction(client, 
-                new LocalAddress(addr), 
+            startInteraction(client.initiator().remoteAddress(new LocalAddress(addr)), 
                 Observable.just(fullHttpRequest()),
                 new Interaction() {
                     @Override
@@ -253,8 +248,7 @@ public class DefaultHttpClientTestCase {
                 enableSSL4Client(),
                 Feature.ENABLE_LOGGING_OVER_SSL);
         try {
-            startInteraction(client, 
-                new LocalAddress(addr), 
+            startInteraction(client.initiator().remoteAddress(new LocalAddress(addr)), 
                 Observable.just(fullHttpRequest()),
                 new Interaction() {
                     @Override
@@ -343,15 +337,15 @@ public class DefaultHttpClientTestCase {
         assertEquals(0, allActiveAllocationsCount(allocator));
         
         try {
-            final Channel ch1 = (Channel)startInteraction(client, 
-                    new LocalAddress(addr), 
+            final Channel ch1 = (Channel)startInteraction(
+                    client.initiator().remoteAddress(new LocalAddress(addr)), 
                     Observable.just(fullHttpRequest()),
                     standardInteraction(allocator, trades)).transport();
             
             assertEquals(0, allActiveAllocationsCount(allocator));
             
-            final Channel ch2 = (Channel)startInteraction(client, 
-                    new LocalAddress(addr), 
+            final Channel ch2 = (Channel)startInteraction(
+                    client.initiator().remoteAddress(new LocalAddress(addr)), 
                     Observable.just(fullHttpRequest()),
                     standardInteraction(allocator, trades)).transport();
             
@@ -386,15 +380,15 @@ public class DefaultHttpClientTestCase {
         assertEquals(0, allActiveAllocationsCount(allocator));
         
         try {
-            final Channel ch1 = (Channel)startInteraction(client, 
-                    new LocalAddress(addr), 
+            final Channel ch1 = (Channel)startInteraction(
+                    client.initiator().remoteAddress(new LocalAddress(addr)), 
                     Observable.just(fullHttpRequest()),
                     standardInteraction(allocator, trades)).transport();
             
             assertEquals(0, allActiveAllocationsCount(allocator));
             
-            final Channel ch2 = (Channel)startInteraction(client, 
-                    new LocalAddress(addr), 
+            final Channel ch2 = (Channel)startInteraction(
+                    client.initiator().remoteAddress(new LocalAddress(addr)), 
                     Observable.just(fullHttpRequest()),
                     standardInteraction(allocator, trades)).transport();
             
