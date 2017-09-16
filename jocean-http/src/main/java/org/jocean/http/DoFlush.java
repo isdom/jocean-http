8131f796d1e9461310a4c378edaa995a45bd11da
ming.ma <isdom.maming@gmail.com>
2017-09-16 18:34:19 +0800

    增加内部实现 FlushOnly 类, 并通过 DoFlush.Util.flushOnly() 返回该类型默认常量

--- jocean-http/src/main/java/org/jocean/http/DoFlush.java
+++ jocean-http/src/main/java/org/jocean/http/DoFlush.java
@@ -1,9 +1,34 @@
 package org.jocean.http;
 
+import io.netty.handler.codec.DecoderResult;
+import io.netty.handler.codec.http.HttpObject;
 
 /**
  * @author isdom
  * Flag Interface for flush when sending
  */
 public interface DoFlush {
+    public static class Util {
+        private static final FlushOnly FLUSH_ONLY = new FlushOnly();
+        
+        public static HttpObject flushOnly() {
+            return FLUSH_ONLY;
+        }
+        
+        private static class FlushOnly implements HttpObject, DoFlush {
+            @Override
+            public DecoderResult decoderResult() {
+                return null;
+            }
+
+            @Override
+            public void setDecoderResult(DecoderResult result) {
+            }
+
+            @Override
+            public DecoderResult getDecoderResult() {
+                return null;
+            }
+        }
+    }
 }
