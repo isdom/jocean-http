95a6d3f4e032f23480d3d259b3a07ebdb85a9122
ming.ma <isdom.maming@gmail.com>
2017-10-10 19:47:54 +0800

    添加特定转换 算子: 从 Observable<HttpObjectWrapper> 转换得到 Observable<FullHttpRequest>

--- jocean-http/src/main/java/org/jocean/http/HttpObjectWrapper.java
+++ jocean-http/src/main/java/org/jocean/http/HttpObjectWrapper.java
@@ -5,12 +5,24 @@ package org.jocean.http;
 
 import org.jocean.idiom.DisposableWrapper;
 
+import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpObject;
+import rx.Observable;
+import rx.Observable.Transformer;
 
 /**
  * @author isdom
  *
  */
 public interface HttpObjectWrapper extends DisposableWrapper<HttpObject> {
-
+    public static class Util {
+        public static Transformer<HttpObjectWrapper,  FullHttpRequest> toFullRequest() {
+            return new Transformer<HttpObjectWrapper,  FullHttpRequest>() {
+                @Override
+                public Observable<FullHttpRequest> call(final Observable<HttpObjectWrapper> obsrequest) {
+                    //  TBD
+                    return null;
+                }};
+        }
+    }
 }
