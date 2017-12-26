12d353f5d52dbf03ba2dff1f1395b24bc4a56035
ming.ma <isdom.maming@gmail.com>
2017-12-26 13:10:29 +0800

    合并 ParamUtil 到 MessageUtil 中

--- jocean-http/src/main/java/org/jocean/http/MessageUtil.java
+++ jocean-http/src/main/java/org/jocean/http/MessageUtil.java
@@ -1,5 +1,8 @@
 package org.jocean.http;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Field;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
@@ -18,8 +21,8 @@ import org.jocean.http.client.HttpClient;
 import org.jocean.http.client.HttpClient.HttpInitiator;
 import org.jocean.http.client.HttpClient.InitiatorBuilder;
 import org.jocean.http.util.Nettys;
-import org.jocean.http.util.ParamUtil;
 import org.jocean.http.util.RxNettys;
+import org.jocean.idiom.Beans;
 import org.jocean.idiom.DisposableWrapper;
 import org.jocean.idiom.DisposableWrapperUtil;
 import org.jocean.idiom.ExceptionUtils;
@@ -27,11 +30,21 @@ import org.jocean.idiom.ReflectUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.deser.DeserializationProblemHandler;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.ByteBufOutputStream;
 import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.handler.codec.http.DefaultHttpRequest;
 import io.netty.handler.codec.http.FullHttpMessage;
+import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.FullHttpResponse;
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpHeaderValues;
@@ -42,8 +55,10 @@ import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpUtil;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http.LastHttpContent;
+import io.netty.handler.codec.http.QueryStringDecoder;
 import io.netty.handler.codec.http.QueryStringEncoder;
 import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.util.CharsetUtil;
 import io.netty.util.ReferenceCountUtil;
 import rx.Observable;
 import rx.Observable.OnSubscribe;
@@ -51,6 +66,7 @@ import rx.Observable.Transformer;
 import rx.Subscriber;
 import rx.functions.Action1;
 import rx.functions.Action2;
+import rx.functions.Func0;
 import rx.functions.Func1;
 import rx.functions.Func2;
 
@@ -62,6 +78,206 @@ public class MessageUtil {
         throw new IllegalStateException("No instances!");
     }
 
+    public static Action1<HttpRequest> injectQueryParams(final Object bean) {
+        return new Action1<HttpRequest>() {
+            @Override
+            public void call(final HttpRequest request) {
+                request2QueryParams(request, bean);
+            }};
+    }
+    
+    public static void request2QueryParams(final HttpRequest request, final Object bean) {
+        final Field[] fields = ReflectUtils.getAnnotationFieldsOf(bean.getClass(), QueryParam.class);
+        if (null != fields) {
+            final QueryStringDecoder decoder = new QueryStringDecoder(request.uri());
+
+            for (Field field : fields) {
+                final String key = field.getAnnotation(QueryParam.class).value();
+                if (!"".equals(key) && null != decoder.parameters()) {
+                    // for case: QueryParam("demo")
+                    injectParamValue(decoder.parameters().get(key), bean, field);
+                }
+                if ("".equals(key)) {
+                    // for case: QueryParam(""), means fill with entire query string
+                    injectValueToField(rawQuery(request.uri()), bean, field);
+                }
+            }
+        }
+    }
+    
+    public static String rawQuery(final String uri) {
+        final int pos = uri.indexOf('?');
+        if (-1 != pos) {
+            return uri.substring(pos+1);
+        } else {
+            return null;
+        }
+    }
+    
+    public static Action1<HttpRequest> injectHeaderParams(final Object bean) {
+        return new Action1<HttpRequest>() {
+            @Override
+            public void call(final HttpRequest request) {
+                request2HeaderParams(request, bean);
+            }};
+    }
+    
+    public static void request2HeaderParams(final HttpRequest request, final Object bean) {
+        final Field[] fields = ReflectUtils.getAnnotationFieldsOf(bean.getClass(), HeaderParam.class);
+        if (null != fields) {
+            for (Field field : fields) {
+                injectParamValue(request.headers().getAll(field.getAnnotation(HeaderParam.class).value()), 
+                    bean,
+                    field
+                );
+            }
+        }
+    }
+    
+    private static void injectParamValue(
+            final List<String> values,
+            final Object obj,
+            final Field field) {
+        if (null != values && values.size() > 0) {
+            injectValueToField(values.get(0), obj, field);
+        }
+    }
+    
+    public static <T> T getAsType(final List<String> list, final Class<T> type) {
+        if (null != list && list.size() > 0) {
+            return Beans.fromString(list.get(0), type);
+        } else {
+            return null;
+        }
+    }
+    
+    /**
+     * @param value
+     * @param obj
+     * @param field
+     */
+    private static void injectValueToField(
+            final String value,
+            final Object obj,
+            final Field field) {
+        if (null != value) {
+            try {
+                field.set(obj, Beans.fromString(value, field.getType()));
+            } catch (Exception e) {
+                LOG.warn("exception when set obj({}).{} with value({}), detail:{} ",
+                        obj, field.getName(), value, ExceptionUtils.exception2detail(e));
+            }
+        }
+    }
+
+    public static <T> Func1<Func0<FullHttpRequest>, T> decodeXmlContentAs(final Class<T> type) {
+        return new Func1<Func0<FullHttpRequest>, T>() {
+            @Override
+            public T call(final Func0<FullHttpRequest> getfhr) {
+                final FullHttpRequest fhr = getfhr.call();
+                if (null != fhr) {
+                    try {
+                        return unserializeAsXml(fhr.content(), type);
+                    } finally {
+                        fhr.release();
+                    }
+                }
+                return null;
+            }};
+    }
+    
+    public static <T> Func1<Func0<FullHttpRequest>, T> decodeJsonContentAs(final Class<T> type) {
+        return new Func1<Func0<FullHttpRequest>, T>() {
+            @Override
+            public T call(final Func0<FullHttpRequest> getfhr) {
+                final FullHttpRequest fhr = getfhr.call();
+                if (null != fhr) {
+                    try {
+                        return unserializeAsJson(fhr.content(), type);
+                    } finally {
+                        fhr.release();
+                    }
+                }
+                return null;
+            }};
+    }
+    
+    public static <T> T unserializeAsXml(final ByteBuf buf, final Class<T> type) {
+        final XmlMapper mapper = new XmlMapper();
+        mapper.addHandler(new DeserializationProblemHandler() {
+            @Override
+            public boolean handleUnknownProperty(final DeserializationContext ctxt, final JsonParser p,
+                    final JsonDeserializer<?> deserializer, final Object beanOrClass, final String propertyName)
+                    throws IOException {
+                LOG.warn("UnknownProperty [{}], just skip", propertyName);
+                p.skipChildren();
+                return true;
+            }});
+        try {
+            return mapper.readValue(contentAsInputStream(buf), type);
+        } catch (Exception e) {
+            LOG.warn("exception when parse xml, detail: {}",
+                    ExceptionUtils.exception2detail(e));
+            return null;
+        }
+    }
+    
+    public static <T> T unserializeAsJson(final ByteBuf buf, final Class<T> type) {
+        try {
+            return JSON.parseObject(contentAsInputStream(buf), type);
+        } catch (IOException e) {
+            LOG.warn("exception when parse {} as json, detail: {}",
+                    buf, ExceptionUtils.exception2detail(e));
+            return null;
+        }
+    }
+    
+    public static String parseContentAsString(final ByteBuf buf) {
+        try {
+            return new String(Nettys.dumpByteBufAsBytes(buf), CharsetUtil.UTF_8);
+        } catch (IOException e) {
+            LOG.warn("exception when parse {} as string, detail: {}",
+                    buf, ExceptionUtils.exception2detail(e));
+            return null;
+        }
+    }
+    
+    private static InputStream contentAsInputStream(final ByteBuf buf) {
+        return new ByteBufInputStream(buf.slice());
+    }
+    
+    public static void serializeToXml(final Object bean, final ByteBuf buf) {
+        final XmlMapper mapper = new XmlMapper();
+//        mapper.addHandler(new DeserializationProblemHandler() {
+//            @Override
+//            public boolean handleUnknownProperty(final DeserializationContext ctxt, final JsonParser p,
+//                    final JsonDeserializer<?> deserializer, final Object beanOrClass, final String propertyName)
+//                    throws IOException {
+//                LOG.warn("UnknownProperty [{}], just skip", propertyName);
+//                p.skipChildren();
+//                return true;
+//            }});
+        try {
+            mapper.writeValue(contentAsOutputStream(buf), bean);
+        } catch (Exception e) {
+            LOG.warn("exception when serialize {} to xml, detail: {}",
+                    bean, ExceptionUtils.exception2detail(e));
+        }
+    }
+    
+    public static void serializeToJson(final Object bean, final ByteBuf buf) {
+        try {
+            JSON.writeJSONString(contentAsOutputStream(buf), CharsetUtil.UTF_8, bean);
+        } catch (IOException e) {
+            LOG.warn("exception when serialize {} to json, detail: {}",
+                    bean, ExceptionUtils.exception2detail(e));
+        }
+    }
+
+    private static OutputStream contentAsOutputStream(final ByteBuf buf) {
+        return new ByteBufOutputStream(buf);
+    }
+    
     private static final Feature F_SSL;
     static {
         F_SSL = defaultSslFeature();
@@ -111,7 +327,7 @@ public class MessageUtil {
         @Override
         public String call(final DisposableWrapper<? extends FullHttpMessage> dwfullmsg) {
             try {
-                return ParamUtil.parseContentAsString(dwfullmsg.unwrap().content());
+                return parseContentAsString(dwfullmsg.unwrap().content());
             } finally {
                 dwfullmsg.dispose();
             }
@@ -642,7 +858,7 @@ public class MessageUtil {
         return decodeContentAs(body.content(), new Func2<ByteBuf, Class<T>, T>() {
             @Override
             public T call(final ByteBuf buf, Class<T> clazz) {
-                return ParamUtil.parseContentAsJson(buf, clazz);
+                return unserializeAsJson(buf, clazz);
             }
         }, type);
     }
@@ -651,7 +867,7 @@ public class MessageUtil {
         return decodeContentAs(body.content(), new Func2<ByteBuf, Class<T>, T>() {
             @Override
             public T call(final ByteBuf buf, Class<T> clazz) {
-                return ParamUtil.parseContentAsXml(buf, clazz);
+                return unserializeAsXml(buf, clazz);
             }
         }, type);
     }
