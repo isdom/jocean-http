a3ad329ac559eba7316f101b64280cd90c199efa
ming.ma <isdom.maming@gmail.com>
2018-04-25 16:09:09 +0800

    issue: https://www.tapd.cn/22048431/bugtrace/bugs/view?bug_id=1122048431001000029 jocean-http 支持文件上传

--- jocean-http/src/main/java/org/jocean/http/ContentUtil.java
+++ jocean-http/src/main/java/org/jocean/http/ContentUtil.java
@@ -1,29 +1,39 @@
 package org.jocean.http;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 import javax.ws.rs.core.MediaType;
 
+import org.jocean.idiom.DisposableWrapper;
+import org.jocean.idiom.DisposableWrapperUtil;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import rx.Observable;
+import rx.functions.Action1;
 import rx.functions.Action2;
 
 public class ContentUtil {
-    
+
     private ContentUtil() {
         throw new IllegalStateException("No instances!");
     }
-    
+
     private final static Action2<Object, OutputStream> _ASXML = new Action2<Object, OutputStream>() {
         @Override
         public void call(final Object bean, final OutputStream os) {
             MessageUtil.serializeToXml(bean, os);
         }};
-    
+
     private final static Action2<Object, OutputStream> _ASJSON = new Action2<Object, OutputStream>() {
         @Override
         public void call(final Object bean, final OutputStream os) {
             MessageUtil.serializeToJson(bean, os);
         }};
-        
+
     public static final ContentEncoder TOXML = new ContentEncoder() {
         @Override
         public String contentType() {
@@ -43,4 +53,27 @@ public class ContentUtil {
         public Action2<Object, OutputStream> encoder() {
             return _ASJSON;
         }};
+
+    public static Observable<? extends MessageBody> tobody(final String contentType, final File file) {
+        try (final InputStream is = new FileInputStream(file)) {
+            final int length = is.available();
+            final byte[] bytes = new byte[length];
+            is.read(bytes);
+            return Observable.just(new MessageBody() {
+                @Override
+                public String contentType() {
+                    return contentType;
+                }
+                @Override
+                public int contentLength() {
+                    return length;
+                }
+                @Override
+                public Observable<? extends DisposableWrapper<ByteBuf>> content() {
+                    return Observable.just(DisposableWrapperUtil.wrap(Unpooled.wrappedBuffer(bytes), (Action1<ByteBuf>)null));
+                }});
+        } catch (final Exception e) {
+            return Observable.error(e);
+        }
+    }
 }
