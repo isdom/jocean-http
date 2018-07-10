04d50fc8f4f2ddfb9029de7764048cec8850287f
ming.ma <isdom.maming@gmail.com>
2018-07-10 10:19:55 +0800

    增加工具方法: HttpSliceUtil.extractHttpMessage 从 HttpSlice 流提取出 HttpMessage 的派生类

--- jocean-http/src/main/java/org/jocean/http/HttpSliceUtil.java
+++ jocean-http/src/main/java/org/jocean/http/HttpSliceUtil.java
@@ -1,9 +1,12 @@
 package org.jocean.http;
 
 import org.jocean.idiom.DisposableWrapper;
+import org.jocean.idiom.DisposableWrapperUtil;
 
+import io.netty.handler.codec.http.HttpMessage;
 import io.netty.handler.codec.http.HttpObject;
 import rx.Observable;
+import rx.Observable.Transformer;
 import rx.functions.Func1;
 
 public class HttpSliceUtil {
@@ -16,4 +19,29 @@ public class HttpSliceUtil {
                     }
                 }));
     }
+
+    public static <T extends HttpMessage> Transformer<HttpSlice, T> extractHttpMessage() {
+        return new Transformer<HttpSlice, T>() {
+            @Override
+            public Observable<T> call(final Observable<HttpSlice> slices) {
+                return slices.flatMap(new Func1<HttpSlice, Observable<? extends DisposableWrapper<? extends HttpObject>>>() {
+                            @Override
+                            public Observable<? extends DisposableWrapper<? extends HttpObject>> call(final HttpSlice slice) {
+                                return slice.element();
+                            }
+                        }).map(DisposableWrapperUtil.unwrap()).first().map(new Func1<HttpObject, T>() {
+                            @SuppressWarnings("unchecked")
+                            @Override
+                            public T call(final HttpObject httpobj) {
+                                if (httpobj instanceof HttpMessage) {
+                                    return (T) httpobj;
+                                } else {
+                                    throw new RuntimeException("First HttpObject is not HttpMessage.");
+                                }
+                            }
+                        });
+            }
+        };
+    }
+
 }
