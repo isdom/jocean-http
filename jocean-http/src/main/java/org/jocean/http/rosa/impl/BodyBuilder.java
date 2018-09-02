5a6d3a545d5e4ece37c158fd34e31258faaeebe3
ming.ma <isdom@qq.com>
2018-09-02 08:22:39 +0200

    移除 SignalClient 定义及相关实现

--- jocean-http/src/main/java/org/jocean/http/rosa/impl/BodyBuilder.java
+++
@@ -1,9 +0,0 @@
-package org.jocean.http.rosa.impl;
-
-import org.jocean.idiom.Ordered;
-
-import rx.functions.Func0;
-
-public interface BodyBuilder extends Func0<BodyForm>, Ordered {
-
-}
--- jocean-http/src/main/java/org/jocean/http/rosa/impl/TypedCacheAware.java
+++
@@ -1,7 +0,0 @@
-package org.jocean.http.rosa.impl;
-
-import org.jocean.idiom.SimpleCache;
-
-public interface TypedCacheAware<V> {
-    public void setTypedCache(final SimpleCache<Class<?>, V> cache);
-}
