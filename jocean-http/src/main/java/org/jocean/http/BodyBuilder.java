2ea4ce88527151760802427d52e21151b3794cd6
ming.ma <isdom@qq.com>
2018-09-12 18:26:41 +0800

    移除过时的 BodyBuilder 定义

--- jocean-http/src/main/java/org/jocean/http/BodyBuilder.java
+++
@@ -1,7 +0,0 @@
-package org.jocean.http;
-
-import rx.Observable;
-
-public interface BodyBuilder {
-    public Observable<? extends MessageBody> build(final Object bean, final ContentEncoder contentEncoder);
-}
