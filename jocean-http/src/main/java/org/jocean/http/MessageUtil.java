727239cfd2fc09898a3687f90b95b45f90c4e3ec
ming.ma <isdom@qq.com>
2018-09-23 22:26:59 +0800

    remove comment code

--- jocean-http/src/main/java/org/jocean/http/MessageUtil.java
+++ jocean-http/src/main/java/org/jocean/http/MessageUtil.java
@@ -356,8 +356,6 @@ public class MessageUtil {
         @Override
         public Observable<String> call(final Interaction interaction) {
             return interaction.execute()
-//                    .compose(AUTOSTEP2DWH)
-//                    .compose(RxNettys.message2fullresp(interaction.initiator(), true))
                     .flatMap(new Func1<FullMessage<HttpResponse>, Observable<DisposableWrapper<? extends ByteBuf>>>() {
                         @Override
                         public Observable<DisposableWrapper<? extends ByteBuf>> call(final FullMessage<HttpResponse> fullresp) {
