7b2abfa4474660896c72fd36adaa09683e95253e
ming.ma <isdom@qq.com>
2018-11-26 21:22:40 +0800

    let compile pass

--- jocean-http/src/main/java/org/jocean/http/RpcExecutor.java
+++ jocean-http/src/main/java/org/jocean/http/RpcExecutor.java
@@ -2,6 +2,7 @@ package org.jocean.http;
 
 import rx.Observable;
 import rx.Observable.Transformer;
+import rx.functions.Func1;
 
 public class RpcExecutor {
     public RpcExecutor(final Observable<RpcRunner> runners) {
@@ -9,7 +10,11 @@ public class RpcExecutor {
     }
 
     public <RESP> Observable<RESP> execute(final Observable<Transformer<RpcRunner, RESP>> rpc2resp) {
-        return rpc2resp.flatMap(invoker -> this._runners.compose(invoker));
+        return rpc2resp.flatMap(new Func1<Transformer<RpcRunner, RESP>, Observable<RESP>>() {
+            @Override
+            public Observable<RESP> call(final Transformer<RpcRunner, RESP> invoker) {
+                return _runners.compose(invoker);
+            }});
     }
 
     private final Observable<RpcRunner> _runners;
