467bed3dddbf9f35876360e591c0add2c3bef297
ming.ma <isdom@qq.com>
2018-11-30 16:16:35 +0800

    对 RpcRunner 增加 name(final String name) 方法

--- jocean-http/src/main/java/org/jocean/http/RpcRunner.java
+++ jocean-http/src/main/java/org/jocean/http/RpcRunner.java
@@ -5,5 +5,6 @@ import rx.functions.Func1;
 
 public interface RpcRunner {
     public RpcRunner spi(final TypedSPI spi);
+    public RpcRunner name(final String name);
     public <T> Observable<T> execute(final Func1<Interact, Observable<T>> invoker);
 }
