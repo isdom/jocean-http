140fd20f86aebeac9a7f527fd0b94a8c4e700382
ming.ma <isdom@qq.com>
2019-05-16 20:48:39 +0800

    add more log for ByteProcessors.IndexOfBytesProcessor

--- jocean-http/src/main/java/org/jocean/netty/util/ByteProcessors.java
+++ jocean-http/src/main/java/org/jocean/netty/util/ByteProcessors.java
@@ -1,11 +1,16 @@
 package org.jocean.netty.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import io.netty.util.ByteProcessor;
 
 public class ByteProcessors {
     private ByteProcessors() {
         throw new IllegalStateException("No instances!");
     }
+
+    private static final Logger LOG = LoggerFactory.getLogger(ByteProcessors.class);
 
     /**
      * A {@link ByteProcessor} which finds the first occurrence of the specified byte array.
@@ -19,11 +24,13 @@ public class ByteProcessors {
         public boolean process(final byte value) {
             if (_bytesToFind[_currentMatchingIdx] == value) {
                 _currentMatchingIdx++;
+                LOG.debug("[{}] == {}", _currentMatchingIdx - 1, value);
                 if (_currentMatchingIdx >= _bytesToFind.length) {
                     return false;
                 }
             }
             else {
+                LOG.debug("[{}]({}) != {}", _currentMatchingIdx, _bytesToFind[_currentMatchingIdx], value);
                 _currentMatchingIdx = 0;
             }
             return true;
