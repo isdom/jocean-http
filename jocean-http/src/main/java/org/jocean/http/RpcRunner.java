8d8aa3391d2ced42dc8fcb9a5b62f9b53a2a4bde
ming.ma <isdom@qq.com>
2019-08-29 14:54:35 +0800

    add oninteract(...) method

--- jocean-http/src/main/java/org/jocean/http/RpcRunner.java
+++ jocean-http/src/main/java/org/jocean/http/RpcRunner.java
@@ -1,10 +1,12 @@
 package org.jocean.http;
 
 import rx.Observable;
+import rx.functions.Action1;
 import rx.functions.Func1;
 
 public interface RpcRunner {
     public RpcRunner spi(final TypedSPI spi);
     public RpcRunner name(final String name);
+    public RpcRunner oninteract(final Action1<Interact> oninteract);
     public <T> Observable<T> execute(final Func1<Interact, Observable<T>> invoker);
 }
