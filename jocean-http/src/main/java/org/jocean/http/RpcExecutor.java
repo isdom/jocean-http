3425c23fcb9c25879dec034fe3b8c5ff11ed2a15
ming.ma <isdom@qq.com>
2020-02-15 21:06:04 +0800

    add public <RESP> Observable<RESP> execute(final Func1<Interact, Observable<RESP>> interact2resp) for RpcExecutor

--- jocean-http/src/main/java/org/jocean/http/RpcExecutor.java
+++ jocean-http/src/main/java/org/jocean/http/RpcExecutor.java
@@ -2,10 +2,12 @@ package org.jocean.http;
 
 import rx.Observable;
 import rx.Observable.Transformer;
+import rx.functions.Func1;
 
 public interface RpcExecutor {
     public <RESP> Observable<RESP> execute(final Observable<Transformer<RpcRunner, RESP>> getrpc2resp);
     public <RESP> Observable<RESP> execute(final Transformer<RpcRunner, RESP> rpc2resp);
+    public <RESP> Observable<RESP> execute(final Func1<Interact, Observable<RESP>> interact2resp);
 
     public <RESP> Observable<RESP> execute(final Transformer<RpcRunner, RpcRunner> runnertransf,
             final Observable<Transformer<RpcRunner, RESP>> getrpc2resp);
--- jocean-http/src/main/java/org/jocean/http/internal/DefaultRpcExecutor.java
+++ jocean-http/src/main/java/org/jocean/http/internal/DefaultRpcExecutor.java
@@ -1,5 +1,6 @@
 package org.jocean.http.internal;
 
+import org.jocean.http.Interact;
 import org.jocean.http.RpcExecutor;
 import org.jocean.http.RpcRunner;
 
@@ -10,16 +11,17 @@ import rx.functions.Func1;
 public class DefaultRpcExecutor implements RpcExecutor {
     @Override
     public <RESP> Observable<RESP> execute(final Observable<Transformer<RpcRunner, RESP>> getrpc2resp) {
-        return getrpc2resp.flatMap(new Func1<Transformer<RpcRunner, RESP>, Observable<RESP>>() {
-            @Override
-            public Observable<RESP> call(final Transformer<RpcRunner, RESP> rpc2resp) {
-                return _runners.compose(rpc2resp);
-            }});
+        return getrpc2resp.flatMap(rpc2resp -> _runners.compose(rpc2resp));
     }
 
     @Override
     public <RESP> Observable<RESP> execute(final Transformer<RpcRunner, RESP> rpc2resp) {
         return _runners.compose(rpc2resp);
+    }
+
+    @Override
+    public <RESP> Observable<RESP> execute(final Func1<Interact, Observable<RESP>> interact2resp) {
+        return _runners.flatMap(runner -> runner.execute(interact2resp));
     }
 
     @Override
