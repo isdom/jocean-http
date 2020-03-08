1c12db09c878b2acaa5bebc1ebc0d0e9c5f374d9
ming.ma <isdom@qq.com>
2020-03-08 11:20:25 +0800

    remove oninteract

--- jocean-http/src/main/java/org/jocean/http/RpcRunner.java
+++ jocean-http/src/main/java/org/jocean/http/RpcRunner.java
@@ -2,13 +2,11 @@ package org.jocean.http;
 
 import rx.Observable;
 import rx.Observable.Transformer;
-import rx.functions.Action1;
 import rx.functions.Func1;
 
 public interface RpcRunner {
     public RpcRunner spi(final TypedSPI spi);
     public RpcRunner name(final String name);
-    public RpcRunner oninteract(final Action1<Interact> oninteract);
     public <T> Observable<T> execute(final Func1<Interact, Observable<T>> invoker);
     public <T> Observable<T> submit(final Transformer<Interact, T> invoker);
 }
