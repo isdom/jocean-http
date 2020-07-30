dfe7db1019f0679f3c4bcefa60464f798aff018a
ming.ma <isdom@qq.com>
2020-07-30 16:03:38 +0800

    限定 RpcScope 只能作用在 ElementType.METHOD 中; 添加 RpcStub 用于标识 RPCBuilder 类型

--- jocean-http/src/main/java/org/jocean/rpc/annotation/RpcStub.java
+++ jocean-http/src/main/java/org/jocean/rpc/annotation/RpcStub.java
@@ -6,7 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface RpcScope {
-    String value();
+@Target(ElementType.TYPE)
+public @interface RpcStub {
 }
