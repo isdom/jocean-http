58a4c4109459cecd887884b55c26183de540e755
ming.ma <isdom@qq.com>
2021-05-31 21:08:39 +0800

    匿名类 lambda 化

--- jocean-http/src/main/java/org/jocean/http/InteractUtil.java
+++ jocean-http/src/main/java/org/jocean/http/InteractUtil.java
@@ -2,21 +2,12 @@ package org.jocean.http;
 
 import java.util.List;
 
-import rx.Observable;
 import rx.functions.Action1;
 
 public class InteractUtil {
     public static InteractPipe applyFeatures(final List<Feature> features) {
-        final Action1<Interact> applier = new Action1<Interact>() {
-            @Override
-            public void call(final Interact interact) {
-                interact.feature(features.toArray(Feature.EMPTY_FEATURES));
-            }};
-        return new InteractPipe() {
-            @Override
-            public Observable<Interact> call(final Observable<Interact> interacts) {
-                return interacts.doOnNext(applier);
-            }};
+        final Action1<Interact> applier = interact -> interact.feature(features.toArray(Feature.EMPTY_FEATURES));
+        return interacts -> interacts.doOnNext(applier);
     }
 
 }
