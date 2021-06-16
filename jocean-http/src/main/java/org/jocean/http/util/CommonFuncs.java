85868e48464d692b4f38ca631be6e7215478b124
ming.ma <isdom@qq.com>
2021-06-16 13:32:29 +0800

    匿名类 lambda 化

--- jocean-http/src/main/java/org/jocean/http/util/CommonFuncs.java
+++ jocean-http/src/main/java/org/jocean/http/util/CommonFuncs.java
@@ -13,23 +13,19 @@ import rx.functions.Action1;
 import rx.functions.Func1;
 
 public class CommonFuncs {
-    private static final Logger LOG =
-            LoggerFactory.getLogger(CommonFuncs.class);
-    
+    private static final Logger LOG = LoggerFactory.getLogger(CommonFuncs.class);
+
     private CommonFuncs() {
         throw new IllegalStateException("No instances!");
     }
-    
-    public static final Func1<Action0, ChannelHandler> ON_CHANNEL_INACTIVE_FUNC1 = 
-            new Func1<Action0, ChannelHandler>() {
-        @Override
-        public ChannelHandler call(final Action0 onChannelInactive) {
-            return new ChannelInboundHandlerAdapter() {
+
+    public static final Func1<Action0, ChannelHandler> ON_CHANNEL_INACTIVE_FUNC1 = onChannelInactive ->
+        new ChannelInboundHandlerAdapter() {
                 @Override
                 public void channelInactive(final ChannelHandlerContext ctx)
                         throws Exception {
                     if (LOG.isDebugEnabled()) {
-                        LOG.debug("ON_CHANNEL_INACTIVE_FUNC1: channel({})/handler({}): channelInactive.", 
+                        LOG.debug("ON_CHANNEL_INACTIVE_FUNC1: channel({})/handler({}): channelInactive.",
                                 ctx.channel(), ctx.name());
                     }
                     ctx.close();
@@ -41,19 +37,14 @@ public class CommonFuncs {
                     }
                 }
             };
-        }
-    };
-    
-    public static final Func1<Action1<Throwable>, ChannelHandler> ON_EXCEPTION_CAUGHT_FUNC1 = 
-            new Func1<Action1<Throwable>, ChannelHandler>() {
-        @Override
-        public ChannelHandler call(final Action1<Throwable> onExceptionCaught) {
-            return new ChannelInboundHandlerAdapter() {
+
+    public static final Func1<Action1<Throwable>, ChannelHandler> ON_EXCEPTION_CAUGHT_FUNC1 = onExceptionCaught ->
+        new ChannelInboundHandlerAdapter() {
                 @Override
                 public void exceptionCaught(final ChannelHandlerContext ctx,
                         final Throwable cause) throws Exception {
-                    LOG.warn("ON_EXCEPTION_CAUGHT_FUNC1: channel({})/handler({}), detail:{}", 
-                            ctx.channel(), 
+                    LOG.warn("ON_EXCEPTION_CAUGHT_FUNC1: channel({})/handler({}), detail:{}",
+                            ctx.channel(),
                             ctx.name(),
                             ExceptionUtils.exception2detail(cause));
                     try {
@@ -63,13 +54,6 @@ public class CommonFuncs {
                     }
                 }
             };
-        }
-    };
-    
-    public static final Func1<SimpleChannelInboundHandler<?>, ChannelHandler> ON_MESSAGE_FUNC1 = 
-            new Func1<SimpleChannelInboundHandler<?>, ChannelHandler>() {
-        @Override
-        public ChannelHandler call(final SimpleChannelInboundHandler<?> handler) {
-            return handler;
-        }};
+
+    public static final Func1<SimpleChannelInboundHandler<?>, ChannelHandler> ON_MESSAGE_FUNC1 = handler -> handler;
 }
